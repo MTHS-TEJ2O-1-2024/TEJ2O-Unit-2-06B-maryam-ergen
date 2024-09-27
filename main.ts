27e126285e81a99debbd617414cb45cdc29dbcbb
maryam-ergen <maryam.ergen@mths.ca>
2024-09-27 10:55:46 -0400

    wrote code

--- main.ts
+++ main.ts
@@ -1,8 +1,20 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Maryam Ergen
+ * Created on: Sep 2024
+ * This program turns LED on a breadboard on and off
 */
 
-basic.showString('Hello, World!')
+basic.clearScreen()
+pins.digitalWritePin(DigitalPin.P16, 0)
+basic.showIcon(IconNames.Happy)
+
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Yes) 
+    pins.digitalWritePin(DigitalPin.P16, 1)
+})
+
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.No)
+    pins.digitalWritePin(DigitalPin.P16, 0)
+})
